47da7a7df13a6ef9bbd0ac81a1191f60b7d37c4a
RasticPack <wolfgreet18@gmail.com>
2026-08-04 14:38:40 +0330

    update 2026-08-04 14:38

--- app/src/main/java/com/rasticpack/app/ui/calc/Calc2Screen.kt
+++ app/src/main/java/com/rasticpack/app/ui/calc/Calc2Screen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
